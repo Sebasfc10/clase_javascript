f3247f633598ee85fb3abb6a58c985634341df7b
Jean Sebastian Torres L <jtorres68@cuc.edu.co>
2021-05-01 11:30:01 -0500

    interacciones en js y conversiones de tipos de datos

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-alert('!Soy JavaScript')
+//interaciones: alert promt, confirm
+
+//alert (ventana modal)
+/*
+alert('hola mundo');
+
+
+//promt
+let nombre_capturado = prompt('Ingrese su nombre : ');
+alert(nombre_capturado);
+*/
+
+//confirm
+/*
+let nombre_capturado = 'te gusta el helado?'
+let gg = confirm(nombre_capturado);
+console.log(gg);
+*/
+//conversion de tipo
+//toString - CONVERTIR EN STRING
+/*
+let valor = true;
+console.log(valor);
+console.log(typeof valor)
+
+
+valor = String(valor);
+console.log(valor);
+console.log(typeof valor)
+*/
+
+
+//CONVERTIR A NUMERO
+
+//alert('6' /  '2');
+/*
+let cadena = 'hi';
+let num_cad = Number(cadena);
+console.log(Number(true))
+*/
+
+//CONVERTIR A BOOLEANO
+console.log(Boolean(' '));
+
+
+
+
+
+
+
+
+
+
