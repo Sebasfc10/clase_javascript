705000e234c8ad4a7ac988a9b87d60b2e139c003
Jean Sebastian Torres L <jtorres68@cuc.edu.co>
2021-05-02 09:43:27 -0500

    bucles y etiquetas

--- app.js
+++ app.js
@@ -41,9 +41,58 @@ console.log(Number(true))
 
 //CONVERTIR A BOOLEANO
 //console.log(Boolean(' '));
+/*
 let edad = 12
 let resultado = edad >= 18 ? `es mayor de edad` : `es menor de edad`;
 console.log(resultado);
+*/
+/*
+let i = 10;
+while(i >0){
+    console.log(`el valor de i es : ${i}`)
+    i--;
+}
+*/
+console.log('++++++++ etiquetas ++++++++++')
+pto: for(i=0 ; i <=10 ; i++){
+    for(j=0 ;j<=10 ; j++){
+        console.log(`la suma de i+j es ${i+j}`)
+        if(j == 2) break pto;
+    }
+}
+
+console.log('salio');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
